e201335defec034328b24bac41c59c5d9e142c25
Jared-Jacobs <54336435+Jared-Jacobs@users.noreply.github.com>
2020-02-16 18:52:10 -0500

    Created UDPServer

---
+++ UDPServer.java
@@ -0,0 +1,19 @@
+/*
+* Jared Jacobs and Josh Coffey
+* This is our UDP Server for the first project for CS-415
+* With this, we will be able to get requests from our client and send CS quotes from a file.
+* Spring 2020
+* @version 1.0
+*/
+
+import java.util.*;
+import java.net.*;
+import java.io.*;
+import java.time.*;
+
+public class UDPServer {
+
+  public static class Server {
+  
+    }
+}
